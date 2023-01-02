548261418d0dd528a0687d025f67c4def5bcdd25
duvrdx <duvrdx@gmail.com>
2023-01-02 18:27:32 -0300

    first commit

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+name = "MusicNet"
+path = "/run/media/duvrdx/BACKUP/musicnet/"
+audios = 330 #len(os.listdir(path+"audios"))
+labels = 330 #len(os.listdir(path+"labels"))
+labels_path = path + "labels"
+audios_path = path + "audios"
